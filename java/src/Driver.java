e5ae867a0d0c5e96f8f68a51cb9ea9c8ed264882
Dylan Lawrence <dlawre14@slu.edu>
2015-04-17 16:37:40 -0500

    fixed java project

---
+++ java/src/Driver.java
@@ -0,0 +1,13 @@
+import classes.structures.containers.ArrayQueue;
+
+/**
+ * Created by Dylan on 4/17/2015.
+ */
+public class Driver {
+
+    public static void main (String[] argv) {
+        ArrayQueue<Integer> q = new ArrayQueue<Integer>();
+
+    }
+
+}
